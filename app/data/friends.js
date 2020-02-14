891391696b7ac6073da79d627f1d4f97ab0f0b4c
Anjali Sharma <anjalishar89@gmail.com>
2020-02-13 18:37:56 -0800

    Adds updated files

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,32 @@
 var friends = [
     {
         name: "Arnold Shortman",
-        photo: "http://pngimg.com/uploads/winnie_pooh/winnie_pooh_PNG37620.png",
+        photo: "https://vignette.wikia.nocookie.net/nickelodeon/images/6/67/Arnold_Shortman-2018.png/revision/latest?cb=20181225170445",
         scores: ["1", "3","5","1","5","5","3","2", "4", "3"]
     },
     {
         name: "Tommy Pickles",
-        photo: "https://i.pinimg.com/originals/d3/2a/ef/d32aef77678f6dbcf5442b567c0b8dfe.png",
+        photo: "https://vignette.wikia.nocookie.net/nickelodeon/images/8/8c/Tommy_Pickles_in_Newer_Years.png/revision/latest?cb=20190607225227",
         scores: ["1", "5","1","5","2","2","2","4", "3", "3"]
     },
     {
-        name: "Rock Rama",
-        photo: "https://www.disneyclips.com/images/images/tigger25.png",
+        name: "Rocko Rama",
+        photo: "https://vignette.wikia.nocookie.net/rockosmodernlife/images/5/5e/Rocko_Wallaby.png/revision/latest?cb=20130920192204",
         scores: ["4", "5","1","1","2","3","3","3", "3", "5"]
     },
     {
         name: "Sandy Cheeks",
-        photo: "https://i.pinimg.com/474x/69/54/d4/6954d481858097417fc50024ee2d02c6--mickey-disney-pooh-bear.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Sandy_Cheeks.svg/1200px-Sandy_Cheeks.svg.png",
         scores: ["1","4","4","4","4","3","3","5", "2", "1"]
     },
     {
         name: "Patty Mayonnaise",
-        photo: "https://i.pinimg.com/originals/f2/9c/9e/f29c9ea29359e9c0b4173164c2d5c5e8.gif",
+        photo: "https://vignette.wikia.nocookie.net/doug/images/1/1e/Patti_Mayonnaise.png/revision/latest?cb=20180531190606",
         scores: ["1","5","3","4","4","4","5","4","3","2"]
     },
     {
         name: "Eliza Thornberry",
-        photo: "https://www.disneyclips.com/images/images/eeyore27.png",
+        photo: "https://vignette.wikia.nocookie.net/nickelodeon/images/9/97/Eliza_Thornberry.png/revision/latest/top-crop/width/360/height/360?cb=20150410060925",
         scores: ["1","3","4","3","3","1","1","5","5","1"]
     },
 
